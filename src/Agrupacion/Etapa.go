c7b5a3cf624a07efd22c1dd646222df671afa1fd
Christianlr <e.chrlr@go.ugr.es>
2023-10-21 00:26:12 +0200

    Añadida estructura principal para el objeto-valor Etapa; closes #11

---
+++ src/Agrupacion/Etapa.go
@@ -0,0 +1,21 @@
+package Etapa
+
+import (
+	Jugador "src/Personas"
+	"time"
+)
+
+type Etapa struct {
+	FechaInicio time.Time
+	FechaFinal  time.Time
+	Jugadores   []Jugador.Jugador
+}
+
+func NuevaEtapa(fechaInicio time.Time, fechaFinal time.Time, jugadores []Jugador.Jugador) Etapa {
+	etapa := Etapa{
+		FechaInicio: fechaInicio,
+		FechaFinal:  fechaFinal,
+		Jugadores:   jugadores,
+	}
+	return etapa
+}
