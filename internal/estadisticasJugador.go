b96a4feeac34d7c731e8f153658d2976615b9537
Christianlr <e.chrlr@go.ugr.es>
2023-10-31 18:49:59 +0100

    Añado entidad estadisticasJugador.go entidad que recoge la parte mutable a normalizar de un jugador; closes #9

---
+++ internal/estadisticasJugador.go
@@ -0,0 +1,15 @@
+package main
+
+type EstadisticasJugador struct {
+	idEstadisticaJugador int
+	perfilBásicoJugador  PerfilBasicoJugador
+	partidosJugados      int
+	puntos               int
+	equipo               string
+	temporada            int
+	asistencias          int
+	rebotes              int
+	tapones              int
+	robos                int
+	perdidas             int
+}
