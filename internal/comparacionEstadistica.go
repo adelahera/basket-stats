0dc137bb13e38e98490082728af4945612531205
adelahera <dlhlalejandro@gmail.com>
2023-11-07 08:44:59 +0100

    Actualizadas las tareas a automatizar closes #23
    Se ha eliminado la automatizacion de las tareas run y build, así como el main para probarlas

--- internal/comparacionEstadistica.go
+++ internal/comparacionEstadistica.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
 type ComparacionEstadistica struct {
 	epocas   map[int]Epoca
 	factores []float64
-}
-
-func main() {
-
-	fmt.Println("Hello, playground")
 }
