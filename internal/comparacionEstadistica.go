76d8d4d2cf6c8dd0917419872012db67e1693e49
adelahera <dlhlalejandro@gmail.com>
2023-11-11 18:00:46 +0100

    Lee archivo CSV closes #26
    Se ha creado una funcion a la que se le indica la ruta del archivo csv a leer y la carga

--- internal/comparacionEstadistica.go
+++ internal/comparacionEstadistica.go
@@ -1,6 +1,31 @@
 package main
 
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
 type ComparacionEstadistica struct {
 	epocas   map[int]Epoca
 	factores []float64
+}
+
+func LeerCSV(nombre string) ([][]string, error) {
+
+	file, err := os.Open(nombre)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	lines, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return lines, err
 }
