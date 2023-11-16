464fa3c93a7c6e52af87e7e0965fefb6be110456
adelahera <dlhlalejandro@gmail.com>
2023-11-16 20:04:31 +0100

    Selección de datos para el cliente closes #30

--- internal/comparacionEstadistica.go
+++ internal/comparacionEstadistica.go
@@ -31,66 +31,33 @@ func LeerCSV(nombre string) ([][]string, error) {
 	return lines, err
 }
 
-func añadeEstadisticas(lines [][]string) map[int]Epoca {
-
-	epocas := make(map[int]Epoca)
-
-	for i := 1950; i < 2020; i += 10 {
-		estadisticasJugadores := make(map[Clave]EstadisticasJugador)
-		for j, line := range lines {
-			if j == 0 || line[1] == "" {
-				continue
-			}
-
-			temporada, err := strconv.Atoi(line[1])
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if temporada >= i && temporada < i+10 {
-				nombre := line[2]
-				clave := Clave{
-					nombreApellidos: nombre,
-					temporada:       temporada,
-				}
-
-				if _, existe := estadisticasJugadores[clave]; existe {
-					continue
-				}
-
-				partidos, _ := strconv.Atoi(line[6])
-				pts, _ := strconv.Atoi(line[52])
-				eq := line[5]
-				asis, _ := strconv.Atoi(line[47])
-				reb, _ := strconv.Atoi(line[46])
-				tap, _ := strconv.Atoi(line[49])
-				rob, _ := strconv.Atoi(line[48])
-				perd, _ := strconv.Atoi(line[50])
-
-				jugador := EstadisticasJugador{
-					nombreApellidos: nombre,
-					partidosJugados: partidos,
-					puntos:          pts,
-					equipo:          eq,
-					temporada:       temporada,
-					asistencias:     asis,
-					rebotes:         reb,
-					tapones:         tap,
-					robos:           rob,
-					perdidas:        perd,
-				}
-
-				estadisticasJugadores[clave] = jugador
-
-			}
-		}
-
-		epocas[i] = Epoca{
-			fechaInicio:           strconv.Itoa(i),
-			fechaFin:              strconv.Itoa(i + 9),
-			estadisticasJugadores: estadisticasJugadores,
+func obtenerIndiceColumna(line []string, nombreColumna string) int {
+	for i, columna := range line {
+		if columna == nombreColumna {
+			return i
 		}
 	}
+
+	return -1
+}
+
+func extraerDatosCSV(lines [][]string, fila int) EstadisticasJugador {
+
+	nombre := lines[fila][obtenerIndiceColumna(lines[0], "Player")]
+	log.Println(obtenerIndiceColumna(lines[0], "Player"))
+	temporada, _ := strconv.Atoi(lines[fila][obtenerIndiceColumna(lines[0], "Year")])
+	partidos, _ := strconv.Atoi(lines[fila][obtenerIndiceColumna(lines[0], "G")])
+	pts, _ := strconv.Atoi(lines[fila][obtenerIndiceColumna(lines[0], "PTS")])
+	eq := lines[fila][obtenerIndiceColumna(lines[0], "Tm")]
+	asis, _ := strconv.Atoi(lines[fila][obtenerIndiceColumna(lines[0], "AST")])
+	reb, _ := strconv.Atoi(lines[fila][obtenerIndiceColumna(lines[0], "TRB")])
+	tap, _ := strconv.Atoi(lines[fila][obtenerIndiceColumna(lines[0], "BLK")])
+	rob, _ := strconv.Atoi(lines[fila][obtenerIndiceColumna(lines[0], "STL")])
+	perd, _ := strconv.Atoi(lines[fila][obtenerIndiceColumna(lines[0], "TOV")])
+
+	return jugador
+}
 	return epocas
+}
+	return jugador
 }
