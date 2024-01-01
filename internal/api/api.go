7deb56380f8fd5f1861dcc02d8931c42a1407b63
adelahera <dlhlalejandro@gmail.com>
2024-01-01 20:24:32 +0100

    Actualiza rutas de la API, closes #71
    Se rediseñan las rutas, orientando las rutas sobre el recurso al que acceden

--- internal/api/api.go
+++ internal/api/api.go
@@ -17,22 +17,28 @@ func SetupApi() *fiber.App {
 func setRoutes() {
 
 	// Devuelve las estadísticas de un jugador
-	app.Get("/estadisticas/:temporada/:jugador", getEstadisticas)
+	app.Get("/estadisticas/:temporada/:nombre", getEstadisticas)
 
-	// Devuelve los jugadores similares a otro en base a una estadística y un umbral de similitud
-	app.Get("/similares/:jugador/:temporada:/:similitud:/:estadistica", getSimilares)
+	// Actualiza las estadisticas de un jugador
+	app.Put("/estadisticas/:temporada/:nombre", updateEstadisticas)
 
-	// Borra un jugador
-	app.Delete("/borrar/:temporada/:jugador", deleteJugador)
+	// Borra las estadísticas de un jugador en una temporada
+	app.Delete("/estadisticas/:temporada/:nombre", deleteEstadisticas)
+
+	// Devuelve los jugadores similares de una epoca a otro en base a una estadística y un umbral de similitud
+	app.Get("/jugador/:temporada:/:nombre/:estadistica/:similitud/:epoca", getSimilares)
+
+	// Borra un jugador de una temporada concreta
+	app.Delete("/jugador/:temporada/:nombre", deleteJugadorTemporada)
+
+	// Borra todos los datos en todas las temporadas de un jugador
+	app.Delete("/jugador/:nombre", deleteJugador)
 
 	// Crea un nuevo jugador
-	app.Post("/crear/:temporada/:jugador", addJugador)
+	app.Post("/jugador", addJugador)
 
 	// Actualiza los datos de un jugador
-	app.Put("/actualizar/:temporada/:jugador", updateJugador)
-
-	// Borra todos los jugadores de una época
-	app.Delete("/borrar/:epoca", deleteEpoca)
+	app.Put("/jugador/:temporada/:nombre", updateJugador)
 
 }
 
@@ -41,6 +47,14 @@ func getEstadisticas(c *fiber.Ctx) error {
 }
 
 func getSimilares(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
+}
+
+func deleteEstadisticas(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
+}
+
+func deleteJugadorTemporada(c *fiber.Ctx) error {
 	return c.SendString("Hello, World 👋!")
 }
 
@@ -56,6 +70,6 @@ func updateJugador(c *fiber.Ctx) error {
 	return c.SendString("Hello, World 👋!")
 }
 
-func deleteEpoca(c *fiber.Ctx) error {
+func updateEstadisticas(c *fiber.Ctx) error {
 	return c.SendString("Hello, World 👋!")
 }
